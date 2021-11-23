e850c5148cd704bc205d85de8d2c2cce4485a83a
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-11-23 15:28:37 -0500

    added new tables and data (teachers and assistance_requests)

--- 3_queries_group-by/5_avg_completion_time.sql
+++ 3_queries_group-by/5_avg_completion_time.sql
@@ -1,5 +1,6 @@
 SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
 FROM students
 JOIN assignment_submissions ON assignment_submissions.student_id  = students.id
+WHERE students.end_date IS NULL
 GROUP BY student
 ORDER BY average_assignment_duration DESC;
